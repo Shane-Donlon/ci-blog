de413907fd81f32bb8d4a36be45d5445f0dbc776
Shane-Donlon <shane.donlon@live.com>
2023-11-08 15:48:36 +0000

    PostList View and context add

--- CiBlog/blog/views.py
+++ CiBlog/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
 def testing(request):
     """testing static dirs setup correctly"""
     return render(request, "blog/blog.html")
+
+class PostList(generic.ListView):
+    model = Post
+    context = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "blog/blog.html"
+    paginate_by = 6
+    context_object_name = "postList"
