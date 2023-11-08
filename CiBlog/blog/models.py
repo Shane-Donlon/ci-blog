918189c3a9f9c1ea26238612ebf3262d393896cc
Shane-Donlon <shane.donlon@live.com>
2023-11-08 13:42:38 +0000

    Models Class Post intial structure

--- CiBlog/blog/models.py
+++ CiBlog/blog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 # Create your models here.
 
 STATUS = ((0, "Draft",), (1,"Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog-posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    featured_image = CloudinaryField("image", default="placeholder")
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
