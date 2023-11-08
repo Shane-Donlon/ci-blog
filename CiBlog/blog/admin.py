6eb9e80801629e9804dc4ba143a4c7e623d8eef2
Shane-Donlon <shane.donlon@live.com>
2023-11-08 14:08:13 +0000

    Summernote installation and config in admin

--- CiBlog/blog/admin.py
+++ CiBlog/blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from . import models
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
-admin.site.register(models.Post)
+
+@admin.register(models.Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ("content")
+    
+
 admin.site.register(models.Comment)
