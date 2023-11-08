f1a476f7977314a06d7354aa6c3f852aed43326a
Shane-Donlon <shane.donlon@live.com>
2023-11-08 14:20:29 +0000

    PostAdmin model setup for admin views

--- CiBlog/blog/admin.py
+++ CiBlog/blog/admin.py
@@ -5,6 +5,10 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(models.Post)
 class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("status", "created_on")
+    list_display = ("title", "slug", "status", "created_on",)
+    search_fields = ["title", "content"]
     summernote_fields = ("content")
     
 
