87147b2585567cb1af320456ce3f1ad2e0e2278d
Shane-Donlon <shane.donlon@live.com>
2023-11-08 13:44:25 +0000

    Model add: Class Methods

--- CiBlog/blog/models.py
+++ CiBlog/blog/models.py
@@ -17,3 +17,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"{self.title}"
+    
+    def number_of_likes(self):
+        return self.likes.count()
