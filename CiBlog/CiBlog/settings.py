842a3e98e9837d919423af282421779745e755e6
Shane-Donlon <shane.donlon@live.com>
2023-11-09 13:59:47 +0000

    AllAuth middleware added

--- CiBlog/CiBlog/settings.py
+++ CiBlog/CiBlog/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -63,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
+
 ]
 
 ROOT_URLCONF = 'CiBlog.urls'
